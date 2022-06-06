c887a05c0bae80568784d2020116e966eb5c43de
dabimarie <100087826+dabimarie@users.noreply.github.com>
2022-06-06 18:33:51 +0700

    Add RecoveryNewPassword

---
+++ frontend/src/views/RecoveryNewPassword.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+    <div class="logo">
+        <div class="logoMetr">
+        </div>
+    </div>
+    <div class="title">
+        <h2>Восстановление пароля</h2>
+    </div>
+    <form action="" method="get" class="form__example">
+      <div class="form__example">
+        <label for="name"></label>
+        <input class="login" type="text" name="name" id="name" placeholder="Новый пароль" required>
+      </div>
+      <div class="form__example">
+        <label for="email"></label>
+        <input class="login" type="email" name="email" id="email" placeholder="Подтвердите пароль" required>
+      </div>
+      <div class="form__example">
+        <input class="form__input" type="submit" value="Сохранить пароль">
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
